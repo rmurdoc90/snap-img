ff290adfa603d8c6ee1c83d08011b922ad1a18e0
rmurdoc90 <rmurdoc90@gmail.com>
2022-07-01 09:51:20 -0700

    first

---
+++ index.js
@@ -0,0 +1,17 @@
+const x = document.getElementById("IMG");
+x.childNodes.forEach((url) => {
+  console.log(url);
+  const newstring = `${url.currentSrc}`;
+  const modString = newstring.slice(83);
+  console.log(modString);
+  url.classList.add(modString);
+});
+let classGrab = "SM-CB-CM-CPLT-6-BLK";
+let step = `${classGrab}.jpeg`;
+let testGrab = document.getElementsByClassName(step);
+console.log(testGrab);
+const y = document.getElementById("IMG-2");
+console.log(testGrab[0]);
+var clone = testGrab[0].cloneNode(true);
+y.appendChild(clone);
+console.log(clone);
